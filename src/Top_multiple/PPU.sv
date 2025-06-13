e7843e9a69024e00777570ae6800930a3b7a1b4f
tzuching0525 <tzuching0525@gmail.com>
2025-06-13 08:56:34 +0800

    Add files via upload

---
+++ src/Top_multiple/PPU.sv
@@ -0,0 +1,50 @@
+// `include "./post_quant.sv"
+// `include "./ReLU_Qint8.sv"
+`define DATA_BITS 32 
+module PPU (
+    input clk,
+    input rst,
+    input i_en,
+    input [`DATA_BITS-1:0] data_in,
+    input [11:0] scaling_factor,
+    output logic[7:0] data_out,
+    output logic valid,
+    output logic [31:0] data_in_out
+);
+
+logic [`DATA_BITS - 1:0] relu_out;
+logic [`DATA_BITS-1:0] data_shift;
+logic [7:0] data_quant, data_temp;
+
+always @(*) begin
+    relu_out = (data_in[`DATA_BITS - 1])? `DATA_BITS'd0 : data_in;
+    // data_shift = (relu_out >>> scaling_factor);
+    data_shift = (relu_out / scaling_factor);
+    data_quant = (data_shift[7:0] ^ 8'd128);
+    data_temp = (!data_quant[7])? 8'd255 : data_quant[7:0];
+end
+
+always @(posedge clk) begin
+    if(rst) begin
+        valid <= 1'd0;
+        data_out <= 8'd0;
+        data_in_out <= 32'd0;
+    end
+    else begin
+        valid <= i_en;
+        data_out <= (i_en)? data_temp : 8'd0;
+        data_in_out <= data_in;
+    end
+end
+
+// ReLU_Qint8 relu(
+//     .data_in(data_in),
+//     .data_out(relu_out)
+// );
+// post_quant post_quant1(
+//     .data_in(relu_out),
+//     .scaling_factor(scaling_factor),
+//     .data_out(data_out)
+// );
+
+endmodule
---
+++ src/Top_multiple/sram_ifmap.sv
@@ -0,0 +1,40 @@
+module sram_ifmap #(
+    parameter SIZE=4096
+)(
+    input logic CLK,
+    input logic [11:0] ADDR,
+    input logic EN,
+    input logic WE,
+    input logic [31:0] DI,
+    output logic [7:0] DO [0:7]
+);
+
+logic [31:0] ram [0:SIZE-1];
+logic [7:0] DO_wire [0:3][0:7];
+
+always @(posedge CLK) begin
+    if (EN) begin
+        DO[0] <= DO_wire[ADDR[1:0]][0];
+        DO[1] <= DO_wire[ADDR[1:0]][1];
+        DO[2] <= DO_wire[ADDR[1:0]][2];
+        DO[3] <= DO_wire[ADDR[1:0]][3];
+        DO[4] <= DO_wire[ADDR[1:0]][4];
+        DO[5] <= DO_wire[ADDR[1:0]][5];
+        DO[6] <= DO_wire[ADDR[1:0]][6];
+        DO[7] <= DO_wire[ADDR[1:0]][7];
+    end
+    if (WE) begin  
+        ram[ADDR] <= DI;
+    end
+end
+
+genvar i, j;
+generate
+    for (i = 0; i < 4; i = i + 1) begin
+        for (j = 0; j < 8; j = j + 1) begin
+            assign DO_wire[i][j] = ram[ADDR[2] * 8 + j][(i+1) * 8 - 1 : i * 8];
+        end
+    end
+endgenerate
+
+endmodule
