602eec53e9ffdf397e62df6fd99149f8f650e991
yuga <yugalin66@gmail.com>
2025-06-23 22:29:48 +0800

    makefile

--- final_lab4/simulation/hardware/Vasic_wrapper__TraceDecls__0__Slow.d
+++
@@ -1,9 +0,0 @@
-Vasic_wrapper__TraceDecls__0__Slow.o: \
- Vasic_wrapper__TraceDecls__0__Slow.cpp \
- /opt/verilator-5.034/share/verilator/include/verilated_vcd_c.h \
- /opt/verilator-5.034/share/verilator/include/verilated.h \
- /opt/verilator-5.034/share/verilator/include/verilatedos.h \
- /opt/verilator-5.034/share/verilator/include/verilated_config.h \
- /opt/verilator-5.034/share/verilator/include/verilated_types.h \
- /opt/verilator-5.034/share/verilator/include/verilated_funcs.h \
- /opt/verilator-5.034/share/verilator/include/verilated_trace.h
--- final_lab4/simulation/hardware/Vasic_wrapper___024root__Slow.d
+++
@@ -1,9 +0,0 @@
-Vasic_wrapper___024root__Slow.o: Vasic_wrapper___024root__Slow.cpp \
- Vasic_wrapper__pch.h \
- /opt/verilator-5.034/share/verilator/include/verilated.h \
- /opt/verilator-5.034/share/verilator/include/verilatedos.h \
- /opt/verilator-5.034/share/verilator/include/verilated_config.h \
- /opt/verilator-5.034/share/verilator/include/verilated_types.h \
- /opt/verilator-5.034/share/verilator/include/verilated_funcs.h \
- Vasic_wrapper__Syms.h Vasic_wrapper.h Vasic_wrapper___024root.h \
- Vasic_wrapper_PE_block.h
